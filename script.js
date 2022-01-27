fc6f8c0be6dc769aa2600349fa96ff6624361cf8
Thibaut Cornil <thibaut.cornil@hotmail.fr>
2022-01-27 15:14:58 +0100

    Envoi du dossier

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new Typed('#typed', {
+    strings: ["Concepteur", "Developpeur", "D'application web et mobile"],
+    typeSpeed: 100,
+    backSpeed: 30,
+    loop: true,
+    showCursor: true
+
+})
+
